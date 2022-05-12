5bfed79676ed91ff0ecf362184ddee7428d519ce
FlaviuPalii <xbuttonyt@gmail.com>
2022-05-12 10:10:50 +0300

    added login page and fixed comment section

--- Lab1TW/js/formComment.js
+++ Lab1TW/js/formComment.js
@@ -1,23 +1,44 @@
 let textInput = document.querySelector(".form-text");
 let submit = document.querySelector(".form-submit");
 let allContents = document.querySelector(".form-all-comments");
+let comments = [];
+// comments = localStorage.getItem("comments") ? localStorage.getItem("comments") : localStorage.setItem("comments", []);
+let valueFromLocalStorage = localStorage.getItem("comments");
+let html = '';
+
+
 let i=0;
 let images=[];
 let time=4500;
 
 allContents.innerHTML = localStorage.getItem("comment");
 
-submit.addEventListener("click", (event) => {
+for(key in comments) {
+    let p = document.createElement('span');
+    allContents.innerHTML = "";
+    allContents.append(comments[key].value, p);
+}
+
+submit.addEventListener("click", (event) => { // Array function
     event.preventDefault();
     if(!textInput.value) {
         alert("Nothing in here... You can't submit this comment!");
         return;
     } else {
-        localStorage.setItem("comment", textInput.value);
-        allContents.innerHTML = textInput.value;
+        let p = document.createElement('span');
+        p.innerHTML = `Guest: ${textInput.value}`;
+        let br = document.createElement('br');
+        allContents.append(br, p);
+        // comments.push({value: textInput.value});
+        // localStorage.setItem("comments", comments);
+
+        // for(key in comments) {
+        //     let p = document.createElement('span');
+        //     let br = document.createElement('br');
+        //     allContents.append(`${comments[key].value}`,br);
+        // }
     }
 });
-
 
 images[0]="slideshow/nismo_slideshow.jpg";
 images[1]="slideshow/gtrs_slideshow.jpg";
@@ -36,3 +57,14 @@ function slideShow() {
 window.onload=function(){
     slideShow();
 }
+
+let loginButton=document.querySelector("#blackgray_text");
+let signForm= document.querySelector(".login-form");
+let bgForm = document.querySelector("#bg-form");
+
+loginButton.addEventListener('click',(e)=>{
+    signForm.style.opacity = 1;
+    signForm.style.visibility = 'visible';
+    bgForm.style.opacity = 0.4;
+    bgForm.style.visibility = 'visible';
+});
--- Lab1TW/js/homepage.js
+++ Lab1TW/js/homepage.js
@@ -1,9 +1,9 @@
 let i=0;
 let images=[];
 let time=1500;
-images[0]="";
-images[1]="";
-images[2]="";
+images[0]="slideshow/nismo_slideshow.jpg";
+images[1]="slideshow/gtrs_slideshow.jpg";
+images[2]="slideshow/silvia_slideshow.jpg";
 function slideShow() {
     document.slideshow.src=images[i];
     if (i<images.length-1) {
@@ -12,4 +12,8 @@ function slideShow() {
         i=0;
     }
     setTimeout("slideShow()", time);
+}
+
+window.onload=function(){
+    slideShow();
 }
